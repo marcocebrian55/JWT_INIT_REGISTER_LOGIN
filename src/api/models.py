777626d587155b49edb851a11173db1a0470a8d5
MARCO CEBRIAN MARIRRODRIGA <marcocebrian55@gmail.com>
2026-02-09 12:20:29 +0000

    nuevo

--- src/api/models.py
+++ src/api/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    
 
     def set_password (self,password):
         self.password = generate_password_hash(password).decode('utf-8')
